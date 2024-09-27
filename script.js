ab28dab89f973e003f6cc1d0e28de669f52650a8
AdimirTen <adimirten@gmail.com>
2024-09-27 18:01:32 +0600

    J.S

---
+++ script.js
@@ -0,0 +1,62 @@
+
+console.log("Hello,world!")
+
+let num = 42
+
+console.log(num)
+
+let isTrue = true
+
+console.log(isTrue)
+
+let a = 5 
+
+let b = 3
+
+console.log(a+b)
+
+const pi = 3.14
+
+console.log(pi)
+
+let name = "Adimir"
+
+console.log(name)
+
+let age = 17
+
+console.log(age)
+
+let message = "Welcome to my website"
+
+console.log(message)
+
+let x = 10
+
+let y = 5
+
+console.log(x-y)
+
+let firstName = "Adimir"
+
+let lastName = "Ten"
+
+console.log(firstName + " " +lastName)
+
+let j = 4 
+
+let p = 8
+
+console.log(j*p)
+
+let greeting = "Hello"
+
+console.log(greeting + " " + name)
+
+let radius = 5
+
+console.log(pi*radius*radius)
+
+let celcia = 10
+
+
